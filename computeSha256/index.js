5c6604ef140ab8e696e3395f38801eef646387fd
Birhanu Digafe <bdigafe2@illinois.edu>
2023-06-24 22:41:37 -0400

    fix hmac

--- computeSha256/index.js
+++ computeSha256/index.js
@@ -1,16 +1,15 @@
-const CryptoJS = require("crypto-js");
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-    
-    //get the data used to sign from request body
-    const data = req.body;
-    const key =  "xVnKuR8elwcDhXalUrGQeiyYRExLl9kl"
+const crypto = require('crypto');
 
-    // sign payload
-    const str = CryptoJS.HmacSHA256(data, key);
-    const sig = CryptoJS.enc.Base64.stringify(str);
+app.use(express.raw({ type: 'application/json' }));
+app.post('/webhook', async (request, response) => {
+    const signature = request.headers['typeform-signature']
+    const isValid = verifySignature(signature, request.body.toString());
+});
 
-    context.res = {   
-        body: sig 
-    };
+const verifySignature = function (receivedSignature, payload) {
+    const hash = crypto
+        .createHmac('sha256', process.env.SECRET_TOKEN)
+        .update(payload)
+        .digest('base64')
+    return receivedSignature === `sha256=${hash}`
 }
