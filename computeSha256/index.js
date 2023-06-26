80beb9d1ecc1b4d501ab8cb74cdd6cdd5811e339
Birhanu Digafe <bdigafe2@illinois.edu>
2023-06-26 16:16:00 -0400

    hmac update

--- computeSha256/index.js
+++ computeSha256/index.js
@@ -7,10 +7,10 @@ module.exports = async function (context, req) {
     const data = req.rawBody.toString('utf8');
     const key =  "xVnKuR8elwcDhXalUrGQeiyYRExLl9kl";
 
-    const hmac = crypto.createHmac('sha256', key);
-    hmac.write(data)
-    hmac.end()      
-    const hash = hmac.read().toString('base64')
+    const hmac = crypto.createHmac('sha256', key)
+        .update(data)
+        .end()      
+    const hash = hmac.digest('base64')
 
     context.res = {   
         body: `sha256=${hash}, len: ${data.length}` 
