d1b81bf92e8ec70282510cce73b6648ed61eaf83
Birhanu Digafe <bdigafe2@illinois.edu>
2023-06-26 17:23:45 -0400

    add base64 output

--- computeSha256/index.js
+++ computeSha256/index.js
@@ -5,6 +5,8 @@ module.exports = async function (context, req) {
     
     //get the data used to sign from request body
     const data = req.rawBody.toString('utf8');
+    const dataBase64 = data.toString('base64');
+
     const key = "e05d2f9a838df65f9da4931471c5c43f67af1e2b309b63a3d57914d9cfadcced";
     //"xVnKuR8elwcDhXalUrGQeiyYRExLl9kl";
 
@@ -13,6 +15,6 @@ module.exports = async function (context, req) {
         .digest('base64')
 
     context.res = {   
-        body: `sha256=${hmac}, len: ${data.length}` 
+        body: `sha256=${hmac}, len: ${data.length}, base64: ${dataBase64}` 
     };
 }
