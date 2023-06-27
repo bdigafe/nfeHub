5d23aec3890faef1431c8b96808c098fdef179a1
Birhanu Digafe <bdigafe2@illinois.edu>
2023-06-26 23:31:58 -0400

    use buffer

--- computeSha256/index.js
+++ computeSha256/index.js
@@ -4,8 +4,8 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     
     //get the data used to sign from request body
-    const data = req.rawBody.toString('utf8');
-    const dataBase64 = data.toString('base64');
+    const data = Buffer.from(req.rawBody, 'utf-8').toString();
+    const dataBase64 = data.toString('hex');
 
     const key = "e05d2f9a838df65f9da4931471c5c43f67af1e2b309b63a3d57914d9cfadcced";
     //"xVnKuR8elwcDhXalUrGQeiyYRExLl9kl";
@@ -15,6 +15,6 @@ module.exports = async function (context, req) {
         .digest('base64')
 
     context.res = {   
-        body: `sha256=${hmac}, len: ${data.length}, base64: ${dataBase64}` 
+        body: `sha256=${hmac}, len: ${data.length}, base64:${dataBase64}` 
     };
 }
