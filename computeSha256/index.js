bc10472584ae0cd2ad1bf236dfd9e596f33deea8
Birhanu Digafe <bdigafe2@illinois.edu>
2023-06-27 01:36:05 -0400

    new key

--- computeSha256/index.js
+++ computeSha256/index.js
@@ -1,20 +1,19 @@
 const crypto = require('crypto');
+const cryptoJs = require('crypto-js');
+
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
     
     //get the data used to sign from request body
-    const data =context.req.rawBody;
-    const dataBase64 = data.toString('hex');
-
-    const key = "e05d2f9a838df65f9da4931471c5c43f67af1e2b309b63a3d57914d9cfadcced";
-    //"xVnKuR8elwcDhXalUrGQeiyYRExLl9kl";
+    const data =Buffer.from(req.rawBody, 'utf8')
+    const key = "xVnKuR8elwcDhXalUrGQeiyYRExLl9kl";
 
     const hmac = crypto.createHmac('sha256', key)
         .update(data)    
         .digest('base64')
 
     context.res = {   
-        body: `sha256=${hmac}, len: ${data.length}, base64:${dataBase64}` 
+        body: `sha256=${hmac}, len: ${data.length}` 
     };
 }
